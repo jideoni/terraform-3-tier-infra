e4d036431ae0909cae8e081b7460e5fe82eab1ae
jideoni <onibabajide34@gmail.com>
2025-09-06 21:36:01 -0500

    enabled encryption for SNS and S3

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -26,8 +26,8 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 
   rule {
     apply_server_side_encryption_by_default {
-      #kms_master_key_id = var.kms_key_arn
-      sse_algorithm = "aws:kms"
+      kms_master_key_id = var.kms_key_arn
+      sse_algorithm     = "aws:kms"
     }
   }
 }
